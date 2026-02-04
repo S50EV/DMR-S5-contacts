79edde2e62f2ad327e358d2369b1fb937d8deeb6
S50EV <57571889+S50EV@users.noreply.github.com>
2026-02-04 18:32:19 +0100

    Create parse_DMR_DB.ps1

---
+++ parse_DMR_DB.ps1
@@ -0,0 +1,6 @@
+Select-String -Path "./users.csv" -Pattern "^293" | ForEach-Object {
+    # Split the line 
+    $Elements = $_.Line.Split(",", [System.StringSplitOptions]::RemoveEmptyEntries)
+    # Construct the string: (Element 0 , Element 1)
+    "$($Elements[0]);$($Elements[1]) $($Elements[2])"
+} | Out-File -FilePath "./contacts.csv" -Encoding utf8
